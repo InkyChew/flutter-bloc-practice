fc6d11c0ac0351c3aa10930c93fa1b78fa918b69
InkyChew <inky11308@gmail.com>
2024-12-13 12:29:05 +0800

    Add weather app

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_repository/user_repository.dart';
 
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/foundation.dart';
 import 'home_page.dart';
+
 import 'login/view/login_page.dart';
 
-void main() {
+Future<void> main() async {
   Bloc.observer = CounterObserver();
+  HydratedBloc.storage = await HydratedStorage.build(
+      storageDirectory: kIsWeb
+          ? HydratedStorage.webStorageDirectory
+          : await getTemporaryDirectory());
   runApp(const App());
 }
 
